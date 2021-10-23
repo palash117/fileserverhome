11013df0b83a99e27192df74e0205bbffb3b6664
unknown <palash2392@gmail.com>
2021-10-24 01:50:20 +0530

    build

--- src/component/MagicastView.js
+++ src/component/MagicastView.js
@@ -2,7 +2,7 @@ import React from 'react'
 import QRCode from "react-qr-code";
 
 const MagicastView = () => {
-    console.log("magicast ip is", process.env.MAGICAST_IP)
+    // console.log("magicast ip is", process.env.MAGICAST_IP)
     return (
         <div style={{
             width: "100%",
@@ -13,6 +13,7 @@ const MagicastView = () => {
             alignItems:"center"
         }}>
         <QRCode value={`http://192.168.1.22`} />
+
            {/* <QRCode value={`http://${process.env.MAGICAST_IP}`} /> */}
         </div>
     )
